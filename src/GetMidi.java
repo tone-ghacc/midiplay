6cafe9ce43ba40b3d783e26154fb5a0d530c18a8
cy22223 <cy22223@shibaura-it.ac.jp>
2024-12-12 16:57:06 +0900

    latest 12/12

--- src/GetMidi.java
+++ src/GetMidi.java
@@ -1,7 +1,37 @@
 import javax.sound.midi.*;
 
 public class GetMidi {
+    public static void printMidiDevices() {
+        MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
+        System.out.println("MidiDevice.Info : " + infos.length + " item(s)");
+        for (int i = 0; i < infos.length; i++) {
+            System.out.println(" MidiDevice.Info[" + i + "]");
+            System.out.println("  Name         : " + infos[i].getName());
+            System.out.println("  Version      : " + infos[i].getVersion());
+            System.out.println("  Vendor       : " + infos[i].getVendor());
+            System.out.println("  Description  : " + infos[i].getDescription());
+            MidiDevice device;
+            try {
+                device = MidiSystem.getMidiDevice(infos[i]);
+            } catch (MidiUnavailableException ex) {
+                throw new IllegalStateException(ex);
+            }
+            System.out.println("  Receivers    : " + device.getMaxReceivers());
+            System.out.println("  Transmitters : " + device.getMaxTransmitters());
+        }
+    }
+
+    public static void playNote(Receiver receiver, int channel, int note, int velocity, int ms)
+            throws InvalidMidiDataException, InterruptedException {
+        ShortMessage sm1 = new ShortMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+        receiver.send(sm1, -1);
+        Thread.sleep(ms);
+        ShortMessage sm2 = new ShortMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+        receiver.send(sm2, -1);
+    }
+
     public static void main(String[] args) {
+        printMidiDevices();
         try {
             // MIDIデバイスの取得
             MidiDevice.Info[] infos = MidiSystem.getMidiDeviceInfo();
@@ -22,7 +52,7 @@ public class GetMidi {
 
             // デバイスをオープン
             device.open();
-
+                      
             // トランスミッターを設定
             Transmitter transmitter = device.getTransmitter();
             transmitter.setReceiver(new Receiver() {
@@ -34,7 +64,7 @@ public class GetMidi {
                             int key = sm.getData1(); // 鍵盤番号
                             int velocity = sm.getData2(); // ベロシティ（力の強さ）
                             if (velocity > 0) {
-                                System.out.println("鍵盤が押されました: " + key);
+                                System.out.println("鍵盤が押されました: " + key+"鍵盤の強さは:" + velocity);
                             }
                         } else if (sm.getCommand() == ShortMessage.NOTE_OFF) {
                             int key = sm.getData1();
