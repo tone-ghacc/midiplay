a192aa6424d49c73d3aab3576575ffb9c0219a1a
tone-gacc <masuharu0823@gmail.com>
2024-12-19 15:34:34 +0900

    #

--- src/MidiRecorder.java
+++ src/MidiRecorder.java
@@ -7,22 +7,22 @@ public class MidiRecorder {
             // シーケンスとトラックを作成
             Sequence sequence = new Sequence(Sequence.PPQ, 24);
             Track track = sequence.createTrack();
-            
+
             // MIDIメッセージ: ノートオン（例: C4）
             ShortMessage noteOn = new ShortMessage();
-            noteOn.setMessage(ShortMessage.NOTE_ON, 0, 60, 93);  // 60はC4のノート番号、93は音量
-            MidiEvent noteOnEvent = new MidiEvent(noteOn, 0);  // 0ティック目にノートオン
+            noteOn.setMessage(ShortMessage.NOTE_ON, 0, 60, 93); // 60はC4のノート番号、93は音量
+            MidiEvent noteOnEvent = new MidiEvent(noteOn, 0); // 0ティック目にノートオン
             track.add(noteOnEvent);
-            
+
             // ノートオフ（例: 500ティック後）
             ShortMessage noteOff = new ShortMessage();
-            noteOff.setMessage(ShortMessage.NOTE_OFF, 0, 60, 0);  // 60はC4、0は音量
-            MidiEvent noteOffEvent = new MidiEvent(noteOff, 500);  // 500ティック後にノートオフ
+            noteOff.setMessage(ShortMessage.NOTE_OFF, 0, 60, 0); // 60はC4、0は音量
+            MidiEvent noteOffEvent = new MidiEvent(noteOff, 500); // 500ティック後にノートオフ
             track.add(noteOffEvent);
-            
+
             // MIDIファイルを保存
             MidiSystem.write(sequence, 1, new java.io.File("output.mid"));
-            
+
             System.out.println("MIDIファイルを保存しました。");
 
         } catch (Exception e) {
